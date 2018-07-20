7ea94090ff3e63ef5ff92550aef3a0a0c9ad713d
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2018-07-20 15:11:34 -0300

    Added new tempalte

--- templates/actions-template.js
+++ templates/actions-template.js
@@ -1,11 +1,27 @@
+const pluralize = require('pluralize');
+
 module.exports = function(variables = {}) {
   const { name, directory } = variables;
+
+  const singularName = pluralize.singular(name);
 
   const data =
 `import {
   GET_${name.toUpperCase()}_REQUEST,
   GET_${name.toUpperCase()}_SUCCESS,
   GET_${name.toUpperCase()}_FAILURE,
+
+  GET_${singularName.toUpperCase()}_REQUEST,
+  GET_${singularName.toUpperCase()}_SUCCESS,
+  GET_${singularName.toUpperCase()}_FAILURE,
+
+  ${singularName.toUpperCase()}_ADD_REQUEST,
+  ${singularName.toUpperCase()}_ADD_SUCCESS,
+  ${singularName.toUpperCase()}_ADD_FAILURE,
+
+  ${singularName.toUpperCase()}_DELETE_REQUEST,
+  ${singularName.toUpperCase()}_DELETE_SUCCESS,
+  ${singularName.toUpperCase()}_DELETE_FAILURE,
 } from ${directory.constants};
 
 export function get${name.capitalize()}Request() {
