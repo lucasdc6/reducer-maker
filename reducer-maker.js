87401baf992a817d623a1f2e3d6fd36f178338b9
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2018-07-23 08:07:42 -0300

    Fix bug

--- reducer-maker.js
+++ reducer-maker.js
@@ -49,11 +49,12 @@ const scan_directory = function() {
   Object.keys(reducersNames).forEach(function(reducer, index) {
     console.log(`Searching ${reducer} directory`);
 
-    ff_result = new ff(reducersNames[reducer], { nottraversal: ['dest'] });
+    ff_result = new ff(reducersNames[reducer], { nottraversal: ['dist'] });
 
     // No reducer directory found
     if (ff_result.length == 0) {
       console.log(`${reducer} not found\n`);
+      reducersDir[reducer] = reducer;
     } else {
       reducersDir[reducer] = ff_result[0];
       console.log(`${reducer} found at '${ff_result}'\n`)
@@ -65,8 +66,9 @@ const make_reducers = function() {
   Object.keys(reducersDir).forEach(function(reducer) {
     let file = `${reducersDir[reducer]}/${args.argv[0]}${reducersSuffix[reducer]}.js`;
     let data = templates[`${reducer}Template`]({name: args.argv[0], directory: reducersDir});
-    let file_path = file.split("/").pop();
-    mkdirp(file_path, function (err) {
+    let file_path = file.split("/");
+    file_path.pop();
+    mkdirp(file_path[0], function (err) {
       if (err) console.error(err);
     });
     fs.writeFile(file, data, function(err) {
